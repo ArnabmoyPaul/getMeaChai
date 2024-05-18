96d2456faa3f8fbfedaba27a7de4ffa00d5e5655
Taranga <122856637+Arnabmoy136@users.noreply.github.com>
2024-05-18 10:08:51 +0600

    first commit

---
+++ GetMeAChai-main/components/Footer.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Footer = () => {
+  return (
+    <footer className='bg-gray-900 text-white flex items-center justify-center px-2 h-10 '>
+      <p className='text-center'>Copyright &copy; Get me A Chai - All rights reserved</p>
+    </footer>
+  )
+}
+
+export default Footer
